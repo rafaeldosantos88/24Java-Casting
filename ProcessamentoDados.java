f45b4107c002e2989a5fe558c53403abd0912a84
Rafael Dos Santos <raphael_979@hotmail.com>
2019-12-27 22:47:53 -0300

    Usando casting para alguns tipos de variaveis

---
+++ ProcessamentoDados.java
@@ -0,0 +1,21 @@
+package rafael;
+
+public class ProcessamentoDados {
+
+	public static void main(String[] args) {
+		
+	int h =5;
+	int b= 5;
+	int B=10;
+	
+	double area = (b+B) / 2 * h; //casting
+	
+	float soma = (float)h + (float)b + B + (int)area;
+	
+	System.out.println(area);
+	System.out.println(soma);
+	
+
+	}
+
+}
